00607f9bcf8dff0c595d761815bdc1e9e1ba070d
Adish Vartak <adishvartak@gmail.com>
2021-04-25 09:02:10 +0200

    Merging Coordinate source files with Location

--- start/src/Location.cc
+++ start/src/Location.cc
@@ -3,6 +3,24 @@
 
 namespace avl {
 
+    Coordinate::Coordinate():
+        line(0),
+        column(0)
+    {
+    }
+
+    Coordinate::Coordinate(const Coordinate& c):
+        line(c.line),
+        column(c.column)
+    {
+    }
+
+    Coordinate::Coordinate(uint32_t l, uint16_t c):
+        line(l),
+        column(c)
+    {
+    }
+
     Location::Location():
         file_index(-1),
         start(),
